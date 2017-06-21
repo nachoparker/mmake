4bb9de7b0775cd93873829bd8cb16bab2ae6ddb0
nacho <nacho.nunez@aoifes.com>
2017-06-21 18:00:04 +0200

    added timecmd and cache stats

--- mmake/run.sh
+++ mmake/run.sh
@@ -18,9 +18,27 @@ export CCACHE_PATH="/usr/bin"
 export CCACHE_DIR=/src/.ccache
 export TERM="xterm"
 
+function timecmd()
+{
+  local BRWN=$'\e[0;33m'
+  local YE=$'\e[1;33m'
+  local NC=$'\e[0m'
+  local START=$( date +%s%N )
+  $@
+  local END=$( date +%s%N )
+  local DELTA=$( expr substr $( echo "( $END - $START )/1000000" | bc -l ) 1 8 )
+  echo -e "\n${BRWN}compilation time ${YE}$DELTA ms${NC}"
+}
+
+
 # COMPILE!
 NICE="nice -n19"
-time exec $NICE make $ARGS -C/src
+timecmd $NICE make $ARGS -C/src
+echo
+echo -e " ────────────"
+echo -e " CCACHE STATS"
+echo -e " ────────────"
+ccache -s | sed '1,3d'
 
 # License
 #
